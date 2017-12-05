85850cc646eb49c56a2e6998b3eab6698b91be82
MHJ <moon5935@gmail.com>
2017-12-05 10:23:57 +0900

    Commit

--- server.js
+++ server.js
@@ -6,20 +6,29 @@ var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+
+    console.log(`${now} ${req.method} ${req.url}`);
+    next();
+});
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
-        welcomeMessage: 'Welcome to my website',
-        currentYear: new Date().getFullYear()
+        welcomeMessage: 'Welcome to my website'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
     });
 });
 
